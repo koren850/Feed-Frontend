8b9831989346621875fbbc88c80c4cb1d087b46c
koren850 <korenah3@gmail.com>
2022-02-08 15:50:25 +0200

    added sockets

--- src/cmps/FeedList.jsx
+++ src/cmps/FeedList.jsx
@@ -1,21 +1,37 @@
 import { useState, useEffect } from "react";
 import { commentService } from "../services/comment.service.js";
+import { socketService } from "../services/socket.service.js";
 
 export const FeedList = () => {
 	const [comments, setComments] = useState([]);
+	let commentsBind;
 
 	useEffect(async () => {
 		const comments = await commentService.query();
 		setComments(comments);
 		console.log(comments);
+		socketService.on("comment-added", commentAdded);
 	}, []);
+
+	async function commentAdded() {
+		const comments = await commentService.query();
+		setComments(comments);
+	}
 
 	if (!comments.length) return <div>no comments yet</div>;
 	return (
 		<section>
 			<input type='text' placeholder='Filter' />
 			<ul>
-				<li> the comments will be displayed here</li>
+				{comments.map((comment) => (
+					<li key={comment._id}>
+						<div>
+							<img style={{ width: "50px" }} src={`https://www.gravatar.com/avatar/${comment.imgHash}?s=300`} />
+						</div>
+						<div>{comment.email}</div>
+						<div>{comment.comment}</div>
+					</li>
+				))}
 			</ul>
 		</section>
 	);
