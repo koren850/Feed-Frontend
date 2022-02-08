7c3899b8079478588bc76c4a5cbda21cfec78a6e
NadavInbar8 <nadavfrc@gmail.com>
2022-02-08 16:18:20 +0200

    added basic layout

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,30 +1,30 @@
-import { useState, useEffect } from "react";
-import { FeedList } from "../cmps/FeedList";
-import { commentService } from "../services/comment.service.js";
+import { useState, useEffect } from 'react';
+import { FeedList } from '../cmps/FeedList';
+import { commentService } from '../services/comment.service.js';
 
 export const Feed = () => {
-	const [comment, setComment] = useState({});
+  const [comment, setComment] = useState({});
 
-	const handleChange = (ev) => {
-		const field = ev.target.name;
-		const value = ev.target.value;
-		setComment({ ...comment, [field]: value });
-	};
+  const handleChange = (ev) => {
+    const field = ev.target.name;
+    const value = ev.target.value;
+    setComment({ ...comment, [field]: value });
+  };
 
-	const submitForm = (ev) => {
-		ev.preventDefault();
-		commentService.add(comment);
-	};
+  const submitForm = (ev) => {
+    ev.preventDefault();
+    commentService.add(comment);
+  };
 
-	return (
-		<section>
-			<form onSubmit={submitForm}>
-				<textarea name='comment' onChange={handleChange}></textarea>
-				<input name='email' onChange={handleChange} type='email' />
-				<button type='submit'>Submit</button>
-			</form>
+  return (
+    <section className='feed'>
+      <form onSubmit={submitForm}>
+        <textarea name='comment' onChange={handleChange}></textarea>
+        <input name='email' onChange={handleChange} type='email' />
+        <button type='submit'>Submit</button>
+      </form>
 
-			<FeedList />
-		</section>
-	);
+      <FeedList />
+    </section>
+  );
 };
