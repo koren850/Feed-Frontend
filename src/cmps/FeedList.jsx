ebe0a6066ef23c373ac8b786c0b800eed0ad58cb
koren850 <korenah3@gmail.com>
2022-02-08 16:19:42 +0200

    added filter

--- src/cmps/FeedList.jsx
+++ src/cmps/FeedList.jsx
@@ -4,7 +4,8 @@ import { socketService } from "../services/socket.service.js";
 
 export const FeedList = () => {
 	const [comments, setComments] = useState([]);
-	let commentsBind;
+	const [filteredComments, setFilteredComments] = useState([]);
+	const [filterBy, setFilterBy] = useState("");
 
 	useEffect(async () => {
 		const comments = await commentService.query();
@@ -13,26 +14,43 @@ export const FeedList = () => {
 		socketService.on("comment-added", commentAdded);
 	}, []);
 
+	useEffect(() => {
+		setFilteredComments(comments);
+	}, [comments]);
+
+	function handleFiller(ev) {
+		setFilterBy(ev.target.value);
+		let filteredComments = comments.filter((comment) => {
+			if (comment.email.toLowerCase().includes(ev.target.value.toLowerCase())) return comment;
+			if (comment.comment.toLowerCase().includes(ev.target.value.toLowerCase())) return comment;
+		});
+
+		setFilteredComments(filteredComments);
+	}
+
 	async function commentAdded() {
 		const comments = await commentService.query();
 		setComments(comments);
 	}
 
-	if (!comments.length) return <div>no comments yet</div>;
 	return (
 		<section>
-			<input type='text' placeholder='Filter' />
-			<ul>
-				{comments.map((comment) => (
-					<li key={comment._id}>
-						<div>
-							<img style={{ width: "50px" }} src={`https://www.gravatar.com/avatar/${comment.imgHash}?s=300`} />
-						</div>
-						<div>{comment.email}</div>
-						<div>{comment.comment}</div>
-					</li>
-				))}
-			</ul>
+			<input onChange={handleFiller} value={filterBy} type='text' placeholder='Filter' />
+			{filteredComments.length ? (
+				<ul>
+					{filteredComments.map((comment) => (
+						<li key={comment._id}>
+							<div>
+								<img style={{ width: "50px" }} src={`https://www.gravatar.com/avatar/${comment.imgHash}?s=300`} />
+							</div>
+							<div>{comment.email}</div>
+							<div>{comment.comment}</div>
+						</li>
+					))}
+				</ul>
+			) : (
+				<div>no comments yet</div>
+			)}
 		</section>
 	);
 };
